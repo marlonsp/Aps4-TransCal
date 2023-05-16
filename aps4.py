cc565cfddcb84d213cfc9ac72c57f12a28c55d2d
Marlon Pereira <marlonsp@al.insper.edu.br>
2023-05-16 16:43:54 -0300

    arquivos

---
+++ aps4.py
@@ -0,0 +1,13 @@
+from funcoesTermosol import importa, plota, geraSaida
+
+#nn -> nº de nós
+#N -> matriz de nós
+#nm -> nº de membros
+#Inc -> matriz de incidência
+#nc -> nº de cargas
+#F -> vetor de carregamento
+#nr -> nº de restrições
+#R -> vetor de restrições
+
+[nn, N, nm, Inc, nc, F, nr, R] = importa('entrada.xlsx')
+
